a674f618fae2163a68ec06c37bc4f239eff47a28
seu11ee <yslee2504@naver.com>
2021-06-28 16:55:15 +0900

    init directories

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express"; // [1]
+const app = express(); // [2]
+import connectDB from "./loader/db";
+
+// Connect Database
+connectDB();
+
+app.use(express.json()); // [3]
+
+// Define Routes
+app.use("/api", require("./api/")); // [4]
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "production" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app // [5]
+  .listen(5000, () => {
+    console.log(`
+    ################################################
+    🛡️  Server listening on port: 5000 🛡️
+    ################################################
+  `);
+  })
+  .on("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
