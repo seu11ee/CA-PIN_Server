a2490a763fb43e2b2c7eaef61c89142ee9d1d115
wonseok <wurijang@gmail.com>
2021-07-02 20:54:23 +0900

    Revert "[feat] add user interface & model"
    This reverts commit 3d4c8066313bcf8dca0ade7d88cacf645174b8bf.

--- src/interfaces/IUser.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-    email: string;
-    password: string;
-    nickname: string;
-    cafeti: string;
-    profileImg: string;
-    created_at: Date;
-    deleted_at: Date;
-}
