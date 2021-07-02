10ec92909d70a3ace31a1581ef187a17afdfb237
seu11ee <yslee2504@naver.com>
2021-07-02 21:11:21 +0900

    [feat] add create Collection code and api for test

--- src/loader/db.ts
+++ src/loader/db.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import config from "../config";
+import Cafe from "../models/Cafe";
+import Review from "../models/Review";
+import Tag from "../models/Tag";
 
 const connectDB = async () => {
   try {
@@ -10,6 +13,19 @@ const connectDB = async () => {
     });
 
     console.log("Mongoose Connected ...");
+
+    Cafe.createCollection().then(function(collection){
+      console.log("Cafe Collection is created!");
+    });
+
+    Review.createCollection().then(function(collection){
+      console.log("Review Collection is created!");
+    });
+
+    Tag.createCollection().then(function(collection){
+      console.log("Tag Collection is created!");
+    });
+
   } catch (err) {
     console.error(err.message);
     process.exit(1);
