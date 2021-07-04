71c6708726f46557ef28ab82e0a1bdce6dafb067
seu11ee <yslee2504@naver.com>
2021-07-04 21:09:08 +0900

    [chore] update dist

---
+++ dist/models/Cafe.js
@@ -0,0 +1,54 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const CafeSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: [mongoose_1.default.SchemaTypes.ObjectId],
+        ref: "Tag"
+    },
+    latitude: {
+        type: Number,
+        required: true
+    },
+    longitude: {
+        type: Number,
+        required: true
+    },
+    instagram: {
+        type: String
+    },
+    opentime: {
+        type: String
+    },
+    opentimeHoliday: {
+        type: String
+    },
+    closetime: {
+        type: String
+    },
+    closetimeHoliday: {
+        type: String
+    },
+    offday: {
+        type: [Number]
+    }
+}, {
+    collection: "cafes"
+});
+exports.default = mongoose_1.default.model("Cafe", CafeSchema);
+//# sourceMappingURL=Cafe.js.map
