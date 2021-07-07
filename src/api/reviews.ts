35f28a287a78c9809e9ff28599b86aa1ac2ce5d1
seu11ee <yslee2504@naver.com>
2021-07-07 17:07:28 +0900

    [feat] add getreviewlist controller

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 const router = express.Router();
-const cafeService = require("../services/cafeService");
+const reviewService = require("../services/reviewService");
 const statusCode = require("../modules/statusCode");
 const responseMessage = require("../modules/responseMessage");
 
@@ -15,6 +15,20 @@ router.get(
     "/:cafeId",
     async(req: Request, res: Response) => {
         const cafeId = req.params.cafeId;
+        try{
+            const reviews = await reviewService(cafeId);
     
+            return res.status(statusCode.OK).json({
+                message:responseMessage.REVIEW_SUCCESS,
+                reviews: reviews
+            });
+        } catch (error) {
+            switch (error.message){
+                case responseMessage.NO_CONTENT:
+                    res.status(statusCode.NO_CONTENT).send();
+                default:
+                    res.status(statusCode.INTERNAL_SERVER_ERROR).send({message:responseMessage.INTERNAL_SERVER_ERROR});
+            }
+        }
     }
 )
