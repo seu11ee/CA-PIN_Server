d4ddcedfcd76a9f38d9e7fed4c6e385192467a4a
wonseok <wurijang@gmail.com>
2021-07-08 14:53:37 +0900

    [fix] error.message change

--- src/api/user.ts
+++ src/api/user.ts
@@ -47,7 +47,7 @@ router.post(
                     res.status(statusCode.BAD_REQUEST).send({message: error.message});
                     break;
                 default:
-                    res.status(statusCode.INTERNAL_SERVER_ERROR).send({message: responseMessage.INTERNAL_SERVER_ERROR});
+                    res.status(statusCode.INTERNAL_SERVER_ERROR).send({message: error.message});
             }
         }
     }
@@ -89,7 +89,7 @@ router.post(
                     res.status(statusCode.BAD_REQUEST).send({message: error.message});
                     break;
                 default:
-                    res.status(statusCode.INTERNAL_SERVER_ERROR).send({message: responseMessage.INTERNAL_SERVER_ERROR});
+                    res.status(statusCode.INTERNAL_SERVER_ERROR).send({message: error.message});
             }
         }
     }
