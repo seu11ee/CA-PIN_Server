a5bb99a700d84b062ef1b98b06cb592f42db90dd
seu11ee <yslee2504@naver.com>
2021-07-10 15:41:18 +0900

    [chore] 주석 제거

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -18,7 +18,6 @@ export default (req, res, next) => {
 
     res.locals.tokenValue = token;
     res.locals.userId = decoded.sub;
-    // req.body.user = decoded.user;
     next();
   } catch (err) {
     next(createError(statusCode.UNAUTHORIZED,responseMessage.EXPIRED_TOKEN));
