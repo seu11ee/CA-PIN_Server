426710c7f50f7ff4f3a86702e754db5c91bcc0d7
wonseok <wurijang@gmail.com>
2021-07-11 14:21:31 +0900

    [feat] add new DTO

--- src/interfaces/ICategory.ts
+++ src/interfaces/ICategory.ts
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
+import {ICafeLocationDTO} from "../interfaces/ICafe";
 export interface ICategory {
     cafes: [mongoose.Types.ObjectId];
     user: mongoose.Types.ObjectId;
     color: string;
     name: string;
     isDefault: boolean;
+}
+
+export interface IMyCafeCategoryDTO {
+    cafes: [mongoose.Types.ObjectId];
+    color: string;
+    name: string;
 }
