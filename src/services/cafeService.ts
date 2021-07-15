eebf574883e87ab513c9794b00a401d8f6cef05f
seu11ee <yslee2504@naver.com>
2021-07-16 04:55:24 +0900

    [fix] 카페 마이맵 태그 필터 추가

--- src/services/cafeService.ts
+++ src/services/cafeService.ts
@@ -45,8 +45,38 @@ const getCafeLocationList = async (tags) => {
     return cafeLocationList;
 }
 
-const getMyMapCafeList = async(userId) => {
-    const mycafeList = await Category.find({user: userId}).populate('cafes','longitude latitude').select("cafes color name");
+const getMyMapCafeList = async(userId,tags) => {
+    const tag_ids = await Tag.find({
+        'tagIdx': { $in: tags
+        }
+    }).select('_id');
+    if (tags.length != tag_ids.length){
+        throw createError(statusCode.BAD_REQUEST,responseMessage.INVALID_IDENTIFIER);
+    }
+    let tagList: mongoose.Types.ObjectId[]= []
+    for (let tag of tag_ids){
+        tagList.push(tag._id);
+    }
+    var mycafeList = []
+    //쿼리에 태그 정보가 없으면 전체 카페 리스트 조회
+    if (tagList.length != 0){
+        mycafeList = await Category.find({user: userId}).populate({
+            path:'cafes',
+            match: { tags : { $all : tagList}},
+            select: "longitude latitude"
+        }).select("cafes color name");
+
+    }
+    //태그로 필터된 카페 리스트 조회
+    else{
+        mycafeList = await Category.find({user: userId}).populate({
+            path:'cafes',
+            select: 'longitude latitude',
+            populate:{
+                path:"tags"
+            }
+        }).select("cafes color name");
+    }
     if (!mycafeList) {
         throw createError(statusCode.NOT_FOUND,responseMessage.INVALID_IDENTIFIER);
     }
